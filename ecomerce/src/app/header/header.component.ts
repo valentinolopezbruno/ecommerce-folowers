15c7fbf48b59a7bfd535b0977b8afe053a9f2522
valentinolopezbruno <valentinolopezbruno@outlook.com>
2023-07-11 20:41:42 -0300

    cambios en admin y home

--- ecomerce/src/app/header/header.component.ts
+++ ecomerce/src/app/header/header.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { APIService } from '../services/api.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  constructor( private APIService: APIService){}
 
+  redes: any[] | null = null;
+  indexCalcularPrecio = 0;
+  usuario: string = '';
+
+  getProductos():void{
+      this.APIService.getProductos().subscribe(data => {this.redes = data , console.log(this.redes)});
+  }
+
+  ngOnInit(){
+      this.getProductos();
+      console.log(this.redes)
+  }
 }
