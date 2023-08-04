484d0039d4470ea687f4fdba9d65770f547d9bc3
ivanhadrowa0@gmail.com <Hadrowa13>
2023-08-04 11:20:12 -0300

    arreglos de boludeces

--- ecomerce/src/app/success/success.component.ts
+++ ecomerce/src/app/success/success.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CarritoService } from '../services/carrito.service';
 
 import { APIService } from '../services/api.service';
+import { Carrito, CarritoItem } from '../models/carrito';
 
 @Component({
   selector: 'app-success',
@@ -9,11 +11,32 @@ import { APIService } from '../services/api.service';
   styleUrls: ['./success.component.css'],
 })
 export class SuccessComponent implements OnInit {
-  constructor(private APIService: APIService, private route: ActivatedRoute) {}
+  constructor(private APIService: APIService, private route: ActivatedRoute, private CarritoService: CarritoService) {}
 
+  carrito: Carrito = { productos: [] };
+
+  carritoFinal: Carrito = { productos: [] };
+
+  datosEntrada: Array<CarritoItem> = [];
+  
   ngOnInit() {
     const token = this.route.snapshot.queryParamMap.get('token');
     console.log('Valor del query param: ', token);
+
+    this.CarritoService.carrito.subscribe((carrito: Carrito) => {
+      this.carrito = carrito;
+
+      this.datosEntrada = this.carrito.productos;
+
+      
+      console.log('carrito');
+      console.log(this.carrito);
+
+      /*console.log('datosEntrada');
+      console.log(this.datosEntrada); */
+    });
+
+
     if (token != null) {
       this.APIService.validarPagoPaypal(token).subscribe({
         next: (res) => {
